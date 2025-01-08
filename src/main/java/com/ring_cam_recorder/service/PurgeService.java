15fd98eb7440bf0e561d85866207c426d20e291b
Serge Blokhin <sergeblokhin@yahoo.com>
2025-01-08 09:34:44 -0500

    refactored some code

--- src/main/java/com/ring_cam_recorder/service/PurgeService.java
+++ src/main/java/com/ring_cam_recorder/service/PurgeService.java
@@ -43,7 +43,7 @@ public class PurgeService {
                         }
                     });
         } catch (IOException e) {
-            logger.error("Error puring", e);
+            logger.error("Error purging", e);
             throw new RuntimeException(e);
         }
     }
